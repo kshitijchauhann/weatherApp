4b9dab3cd041b718483efe1dffcea4bc70c068cd
username <chauhankay18@gmail.com>
2024-04-24 21:05:53 +0530

    Built a basic working app

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const city = document.getElementById('city');
 const time = document.getElementById('time');
 const tempCelcius = document.getElementById('temp-cel');
-const tempFarenheit = document.getElementById('temp-far');
+const errorMessage = document.getElementById('error');
+const feel = document.getElementById('feelslike');
 
-async function weather() {
+async function weather(cityName) {
     try {
 
-        const response = await fetch('http://api.weatherapi.com/v1/current.json?key=ee197a4b155b4d3d843153413241904&q=udaipur');
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ee197a4b155b4d3d843153413241904&q=${cityName}`);
         const data = await response.json();
         console.log(data);
         city.textContent = `City: ${data.location.name}`;
         time.textContent = `Time: ${data.location.localtime}`;
-        tempCelcius.textContent = `Temperature(Celcius): ${data.current.temp_c}`;
-        tempFarenheit.textContent = `Temperature(Farenheit): ${data.current.temp_f}`;
+        tempCelcius.textContent = `Temperature: ${data.current.temp_c} C`;
+        feel.textContent = `Feels like ${data.current.feelslike_c} C`;
     } catch(error) {
         console.log(error);
+        throw new Error('City not found. Please enter a valid city name.');
     }
 }
 
-weather()
+
+async function searchComplete(cityName) {
+        const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=ee197a4b155b4d3d843153413241904&q=${cityName}`);
+        const data = await response.json();
+        console.log(data);
+
+}
+
+
+document.getElementById('search-button').addEventListener('click', async () => {
+    try {
+        const cityName = document.getElementById('search').value;
+        await weather(cityName);
+        await searchComplete(cityName);
+    } catch(error) {
+        errorMessage.textContent = `${error}`;
+    }
+});
